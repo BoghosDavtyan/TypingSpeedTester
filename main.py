b1cc82a6653244fced3dff78c8b78edac7b95c52
BoghosDavtyan <127856427+BoghosDavtyan@users.noreply.github.com>
2024-07-29 22:11:59 +0300

    Adding red blink when you enter the wrong word

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.clock import Clock
+from kivy.graphics import Color, Rectangle
 import random
+
+
+class BlinkingTextInput(TextInput):
+    def blink(self, dt):
+        self.background_color = [1, 0, 0, 1] if self.background_color == [1, 1, 1, 1] else [1, 1, 1, 1]
 
 
 class TypeTestApp(App):
@@ -15,6 +21,7 @@ class TypeTestApp(App):
         self.time_left = 60
         self.words_typed = 0
         self.accuracy = 100
+        self.game_active = False
 
         layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
 
@@ -24,7 +31,7 @@ class TypeTestApp(App):
         self.word_label = Label(text="Press 'Start' to begin", font_size=30)
         layout.add_widget(self.word_label)
 
-        self.input = TextInput(multiline=False, font_size=30)
+        self.input = BlinkingTextInput(multiline=False, font_size=30, disabled=True)
         self.input.bind(on_text_validate=self.check_word)
         layout.add_widget(self.input)
 
@@ -39,9 +46,11 @@ class TypeTestApp(App):
     def start_game(self, instance):
         self.start_button.disabled = True
         self.input.disabled = False
+        self.input.focus = True
         self.time_left = 60
         self.words_typed = 0
         self.accuracy = 100
+        self.game_active = True
         self.next_word()
         Clock.schedule_interval(self.update_timer, 1)
 
@@ -50,14 +59,32 @@ class TypeTestApp(App):
         self.word_label.text = self.current_word
 
     def check_word(self, instance):
+        if not self.game_active:
+            return
+
         typed_word = self.input.text.strip().lower()
         if typed_word == self.current_word:
             self.words_typed += 1
+            self.input.text = ""
+            self.next_word()
         else:
             self.accuracy = round((self.words_typed / (self.words_typed + 1)) * 100, 2)
+            self.input.text = ""
+            self.blink_textinput()
         self.update_stats()
-        self.input.text = ""
-        self.next_word()
+        self.input.focus = True
+
+    def blink_textinput(self):
+        blink_count = 0
+
+        def blink(dt):
+            nonlocal blink_count
+            self.input.blink(dt)
+            blink_count += 1
+            if blink_count >= 6:
+                return False
+
+        Clock.schedule_interval(blink, 0.2)
 
     def update_timer(self, dt):
         self.time_left -= 1
@@ -71,6 +98,7 @@ class TypeTestApp(App):
         self.stats_label.text = f"Words typed: {self.words_typed} | Accuracy: {self.accuracy}%"
 
     def end_game(self):
+        self.game_active = False
         self.input.disabled = True
         self.word_label.text = "Game Over!"
         self.start_button.disabled = False
